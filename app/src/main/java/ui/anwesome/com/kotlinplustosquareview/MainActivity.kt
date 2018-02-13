e82619331d45a5b54e262f34a22ce4bdb40a4220
Anwesh43 <anweshthecool0@gmail.com>
2018-02-14 01:32:34 +0530

    adding PlusToSquareListener in activity

--- app/src/main/java/ui/anwesome/com/kotlinplustosquareview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinplustosquareview/MainActivity.kt
@@ -2,12 +2,18 @@ package ui.anwesome.com.kotlinplustosquareview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.plustosquareview.PlusToSquareView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        PlusToSquareView.create(this)
+        val view = PlusToSquareView.create(this)
+        view.addPlusToSquareListener({
+            Toast.makeText(this, "square created", Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this, "plus created", Toast.LENGTH_SHORT).show()
+        })
     }
 }
