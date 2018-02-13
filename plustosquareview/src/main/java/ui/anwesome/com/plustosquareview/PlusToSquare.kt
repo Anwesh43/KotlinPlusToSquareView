0318f4e22ed7406188ce2affff996ad4ba2360ad
Anwesh43 <anweshthecool0@gmail.com>
2018-02-14 00:34:14 +0530

    adding PlusToSquareView class

--- plustosquareview/src/main/java/ui/anwesome/com/plustosquareview/PlusToSquare.kt
+++ plustosquareview/src/main/java/ui/anwesome/com/plustosquareview/PlusToSquare.kt
@@ -3,3 +3,22 @@ package ui.anwesome.com.plustosquareview
 /**
  * Created by anweshmishra on 14/02/18.
  */
+
+import android.content.*
+import android.graphics.*
+import android.view.*
+
+class PlusToSquareView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
